4900c0e844be7a4a104cecafe06eccb4b652ff12
ubuntu2 <meteorcomm@gmail.com>
2021-06-25 15:25:03 +0300

    first commit

--- detect.py
+++ detect.py
@@ -1,5 +1,8 @@
 import os
 # comment out below line to enable tensorflow outputs
+from tensorflow.core.protobuf.config_pb2 import ConfigProto
+from tensorflow.python.client.session import InteractiveSession
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -14,8 +17,8 @@ from tensorflow.python.saved_model import tag_constants
 from PIL import Image
 import cv2
 import numpy as np
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
 
 flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
 flags.DEFINE_string('weights', './checkpoints/yolov4-416',
